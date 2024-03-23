46d1312f695ff6e7f259e9d83408e4b71e3ba576
Nikolay <73880661+ngdimitrov@users.noreply.github.com>
2024-03-24 01:36:34 +0200

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     lxd = {
-      source = "terraform-lxd/lxd"
+      source  = "terraform-lxd/lxd"
       version = "2.0.0"
     }
   }
@@ -12,33 +12,31 @@ provider "lxd" {
 }
 
 resource "lxd_instance" "nginx" {
-  name  = "nginx"
-  image = "ubuntu:22.04"
+  name             = "nginx"
+  image            = "ubuntu:22.04"
   wait_for_network = "true"
 }
-
 
 resource "lxd_instance" "wiki" {
-  name  = "wiki"
-  image = "ubuntu:22.04"
+  name             = "wiki"
+  image            = "ubuntu:22.04"
   wait_for_network = "true"
 }
 
-
 resource "lxd_instance" "wiki2" {
-  name  = "wiki2"
-  image = "ubuntu:22.04"
+  name             = "wiki2"
+  image            = "ubuntu:22.04"
   wait_for_network = "true"
 }
 
 resource "lxd_instance" "db" {
-  name  = "db"
-  image = "ubuntu:22.04"
+  name             = "db"
+  image            = "ubuntu:22.04"
   wait_for_network = "true"
 }
 
 resource "lxd_instance" "github" {
-  name  = "github"
-  image = "ubuntu:22.04"
+  name             = "github"
+  image            = "ubuntu:22.04"
   wait_for_network = "true"
 }
