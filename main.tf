2ced7537b298c0b850ddfb2f7f9a00fc4abb7e4c
Nikolay <73880661+ngdimitrov@users.noreply.github.com>
2024-03-28 11:16:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,28 @@ provider "lxd" {
   # Configuration options
 }
 
+resource "lxd_network" "internal" {
+  name = "internal"
+
+  config = {
+    "ipv4.address" = "10.206.7.1/24"
+  }
+}
+
 resource "lxd_instance" "nginx" {
   name             = "nginx"
   image            = "ubuntu:22.04"
   wait_for_network = "true"
+}
+
+  device {
+    name = "http"
+    type = "nginx"
+    properties = {
+      listen = "tcp:0.0.0.0:80"      
+      connect = "tcp:10.206.7.64:80"
+    }
+  }
 }
 
 resource "lxd_instance" "wiki" {
